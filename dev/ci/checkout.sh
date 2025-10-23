ac599a6cb65f9255ff28c2dc69912e6c78711427
Rodolphe Lepigre <rodolphe.lepigre@skylabs-ai.com>
2025-10-23 16:34:08 +0200

    Add [dev/ci/checkout.sh] script.

---
+++ dev/ci/checkout.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# This script is used to checkout specific commits for all the repositories in
+# the workspace, including for the workspace repository itself.
+#
+# It expects a single command-line argument: a path to the file specifying the
+# commits for each repository of the workspace. Each of its lines should be of
+# the form "<REPO_DIR>/: <COMMIT_HASH>".
+#
+# NOTE: the script should be invoked from the workspace root.
+#
+
+set -euf -o pipefail
+
+if [[ $# -ne 1 ]]; then
+  echo "Error: a single argument is expected (path to the commit config)."
+  exit 1
+fi
+
+export COMMITS_FILE="$1"
+
+if [[ ! -f "${COMMITS_FILE}" ]]; then
+  echo "Error: file ${COMMITS_FILE} does not exist."
+  exit 1
+fi
+
+if [[ "$(make loop LOOP_COMMAND=echo | wc -l)" != "1" ]]; then
+  echo "Error: sub-repositories are already cloned."
+  exit 1
+fi
+
+export LOOP_COMMAND="dev/ci/checkout_command.sh"
+
+# Checkout the specified commit on the workspace repository.
+make loop-workspace
+
+# Clone all the sub-repos (shallowly).
+make clone -j CLONE_ARGS="--depth 1 --quiet"
+
+# Checkout the specified commit on the sub-repositories.
+make loop-subrepos -j
---
+++ dev/ci/checkout_command.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# This script is relied on by [checkout_command.sh] (via LOOP_COMMAND).
+#
+
+set -euf -o pipefail
+
+if [[ $# -ne 4 ]]; then
+  echo "Error: four command line arguments expected."
+  exit 1
+fi
+
+REPO_DIR="$4"
+
+if [[ -z "${COMMITS_FILE}" ]]; then
+  echo "Error: the COMMITS_FILE environment is undefined or empty."
+  exit 1
+fi
+
+REPO_HASH=$(grep "^${REPO_DIR}: " ${COMMITS_FILE} | cut -d ' ' -f 2)
+
+git -C "${REPO_DIR}" fetch --depth 1 --quiet origin "${REPO_HASH}"
+git -C "${REPO_DIR}" -c advice.detachedHead=false checkout "${REPO_HASH}"
