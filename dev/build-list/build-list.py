7f84eb0c33ea4e375e36e3ec117ff63141816ea5
Jan-Oliver Kaiser <janno@skylabs-ai.com>
2025-11-05 10:57:11 +0100

    Convert build-list.py to stand-alone uv script
    This allows us to typecheck it independently using
    ```
    uvx --with-requirements build-list.py ty check build-list.py
    ```

--- dev/build-list/build-list.py
+++ dev/build-list/build-list.py
@@ -1,4 +1,11 @@
-#! /usr/bin/env python3
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = "==3.12.*"
+# dependencies = [
+#     "pyyaml>=6.0.3",
+#     "sexpdata>=1.0.2",
+# ]
+# ///
 
 import yaml
 import os
