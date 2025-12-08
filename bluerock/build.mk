939daf50995020f6de95243564067d44b78e3a43
Jan-Oliver Kaiser <janno@skylabs-ai.com>
2025-12-08 11:26:47 +0100

    Avoid bhv-specific venv entirely.

--- bluerock/build.mk
+++ bluerock/build.mk
@@ -1,20 +1,15 @@
 BHV_DIR = bluerock/bhv
 GITLAB_URL="${GITHUB_URL}SkyLabsAI/bluerock."
-BHV_VENV = ${BHV_DIR}/.venv
 
 .PHONY: ast-prepare-bhv
 ast-prepare-bhv: ide-prepare
 ifeq ($(wildcard ${BHV_DIR}),${BHV_DIR})
-ifneq ($(wildcard ${BHV_VENV}),${BHV_VENV})
-	@echo "[ENV] ${BHV_VENV}"
-	$(Q)(cd ${BHV_DIR}; uv venv)
-	$(Q)ls ${BHV_VENV} > /dev/null
-endif
-	@echo "[PIP] ${BHV_DIR}/python_requirements.txt"
-	$(Q)(cd ${BHV_DIR}; uv pip install -r python_requirements.txt)
 	@echo "[AST] ${BHV_DIR}"
-	+$(Q)(cd ${BHV_DIR}; LLVM=1 BUILD_CACHING=0 SHALLOW=1 \
-		GITLAB_URL=${GITLAB_URL} uv run -- ./fm-build.py -b)
+	+$(Q)(LLVM=1 BUILD_CACHING=0 SHALLOW=1 \
+		GITLAB_URL=${GITLAB_URL} \
+		uv --directory ${BHV_DIR} run \
+		   --with-requirements python_requirements.txt \
+		   --no-project --isolated -- ./fm-build.py -b)
 else
 	@echo "Skipping AST generation for ${BHV_DIR} (not cloned)."
 endif
